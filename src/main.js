d31890f496bfc5d3edc0a09d8acf638b76c912a0
bucho86 <lovenwar101@gmail.com>
2024-10-09 14:35:29 -0400

    add origal files back to my main,js file

--- src/main.js
+++ src/main.js
@@ -1,9 +1,36 @@
-import './style.css'
+ import './style.css'
 // import javascriptLogo from '../javascript.svg'
 // import viteLogo from '/vite.svg'
 // import {setupCounter} from '../counter.js'
+const postEl =document.querySelector('#posts');
+const API_URL = "https://bkpybdqjxmrgxwaqmxqe.supabase.co/rest/v1"
+const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrcHliZHFqeG1yZ3h3YXFteHFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0MjU5MTgsImV4cCI6MjAyNjAwMTkxOH0.1kLMTU1ZYusSamYBWHqs--GDYU-NGeYrX7qy4bNLTZU"
+ 
+ fetch(`${API_URL}/f24_posts?select=*&apikey=${API_KEY}`)
+.then(reponse=> response.json())
+.then(data => {
+    data. forEach(item => showPostWebView(item))
+})
+function showPostWebView(item = {}) {
+    const div = document .createElement('div');
+    div.innerHTML = `
+    <div class ="card bg-base-200 shadow-x1">
+        <div class="card-body">
+        <h3 class ="text-gray-800">Published on:
+    ${item['pulishedOn']} by ${item['publishedBy']}</p>
+            <p>${item['content']}</p>
+            <div class="card-action justify-end">
+            <botton class="btn btn-promary">Read
+  More</button>
+                     </div>
+                 </div>
+            </div>
 
-document.querySelector('#app').innerHTML = `
+    `
+    postEl.appendChild(div);
+}
+
+ document.querySelector('#app').innerHTML = `
   <div class="navbar bg-base-100">
     <div class="navbar-start">
         <div class="dropdown">
@@ -101,4 +128,5 @@ document.querySelector('#app').innerHTML = `
     <div class="container mx-auto p-4 text-center">
         <p>&copy; 2024 Blog Builder Project. All rights reserved.</p>
     </div>
-</footer>`
+</footer>`;
+
